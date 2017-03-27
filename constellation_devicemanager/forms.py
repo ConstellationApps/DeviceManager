2c464e34983aaf4436af3c13726c7ecdeedff87c
Matthew Wynn <matthew@matthewwynn.com>
2017-03-27 01:13:12 -0500

    Add hostname regex validation to frontend

--- constellation_devicemanager/forms.py
+++ constellation_devicemanager/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm
 
+from . import validators
+
 from .models import Device
 
 class DeviceForm(ModelForm):
@@ -7,6 +9,7 @@ class DeviceForm(ModelForm):
         super(DeviceForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'mdl-textfield__input'
+        self.fields['hostname'].widget.attrs['pattern'] = validators.hostname
     class Meta:
         model = Device
         fields = ['MAC', 'name', 'owner', 'hostname']
