7477e8a7dbcb07f7bc2f775df3e90ccc84b10331
Matthew Wynn <matthew@matthewwynn.com>
2017-04-17 02:04:10 -0500

    Use namespaced urls

--- constellation_devicemanager/urls.py
+++ constellation_devicemanager/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'constellation_devicemanager'
 urlpatterns = [
     url(r'^view/user$', views.view_show_user,
         name="view_show_user"),
